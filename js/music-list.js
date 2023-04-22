e075780f100a66a4c005088b9483179e1dbecbc5
lakhan singh rathore <107997584+lakhan0007@users.noreply.github.com>
2023-04-22 12:38:31 +0530

    MusicPlayer_Update

---
+++ js/music-list.js
@@ -0,0 +1,104 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+      name: "Aise kyu",
+      artist: "Anurag-Saikia",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Hanuman Chalisa",
+      artist: "Gulshan Kumar",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Hum katha sunate",
+      artist: "Ravindra Jain",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Kho gaye hum kaha",
+      artist: "Taaruk Raina",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Phir se udd chala",
+      artist: "A-R Rehman",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Pyaar-Pyaar",
+      artist: "Dino James",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "Ram-Ram",
+      artist: "MC Square",
+      img: "music-7",
+      src: "music-7"
+    },
+    {
+      name: "Unstopable",
+      artist: "Dino James",
+      img: "music-8",
+      src: "music-8"
+    },
+    {
+      name: "Wishlist",
+      artist: "Dino James",
+      img: "music-9",
+      src: "music-9"
+    },
+    {
+      name: "BYE",
+      artist: "Dino James",
+      img: "music-9",
+      src: "music-10"
+    },
+    {
+      name: "Zindgi",
+      artist: "Dino James",
+      img: "music-9",
+      src: "music-11"
+    },
+    {
+      name: "Yalgar",
+      artist: "Carry Minati",
+      img: "music-9",
+      src: "music-12"
+    },
+    {
+      name: "Yadav Brand",
+      artist: "Dino James",
+      img: "music-9",
+      src: "music-13"
+    },
+    {
+      name: "Pyar",
+      artist: "Abdu",
+      img: "music-9",
+      src: "music-14"
+    },
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
